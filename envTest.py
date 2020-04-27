00f963930a9ea57e75d1757552498a4260e5bfc8
Soralink <qitianye1104@gmail.com>
2020-04-27 18:58:47 -0400

    envtest

--- envTest.py
+++ envTest.py
@@ -1,5 +1,19 @@
-def main():
-    print('')
+import gym
+import pybulletgym
 
-if __name__ == '__main':
+def main():
+    env = gym.make("HopperPyBulletEnv-v0")
+    env.render("human")
+    env.reset()
+    while True:
+        action = env.action_space.sample()
+        o,r,d,i = env.step(action)
+        if d:
+            env.reset()
+
+
+    
+
+if __name__ == '__main__':
+    print('test')
     main()
