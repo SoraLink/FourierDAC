251b08e20f6aad08dc01e0334741642c3b9c222a
Soralink <qitianye1104@gmail.com>
2020-04-28 18:11:56 -0400

    logger

--- util/logger.py
+++ util/logger.py
@@ -4,36 +4,44 @@ from datetime import datetime
 
 class logger:
     def __init__(self, columns):
-        self.columns = columns
-        self.data_storage = [0]*len(columns)
-        self.nameOfFolder = datetime.now()
-        self.path = os.getcwd()+'\\'+self.nameOfFolder
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-        self.file_directory = self.path+'\\log.csv'
-        self.file = open(self.file_directory,'wb')
-        self.episode = 0
+        self._columns = columns
+        self._data_storage = [0]*len(columns)
+        self._nameOfFolder = str(datetime.now().strftime("%Y%m%d-%H%M%S"))
+        self._path = os.getcwd()+'\\'+'data'+'\\'+self._nameOfFolder
+        self._path.replace(' ','')
+        if not os.path.exists(self._path):
+            os.makedirs(self._path)
+        self._file_directory = self._path+'\\log.csv'
+        self._file = open(self._file_directory,'w', newline='')
+        self.write_column_name()
+        self._episode = 0
 
     def log(self, dataset):
         for i, data in enumerate(dataset):
-            self.data_storage[i]+=data
-        self.episode+=1
+            self._data_storage[i]+=data
+        self._episode+=1
     
     def dump(self):
-        filewriter = csv.writer(self.file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for i in len(self.data_storage):
-            self.data_storage[i]/=self.episode
-        filewriter.writerow(self.data_storage)
-        self.data_storage=[0]*len(self.columns)
-        self.episode=0
+        filewriter = csv.writer(self._file)
+        row = []
+        for i in range(len(self._data_storage)):
+            row.append(self._data_storage[i]/self._episode)
+        filewriter.writerow(row)
+        self._data_storage=[0]*len(self._columns)
+        self._episode=0
 
     def write_column_name(self):
-        filewriter = csv.writer(self.file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(self.columns)
+        filewriter = csv.writer(self._file)
+        filewriter.writerow(self._columns)
 
+
+def logger_test():
+    log = logger(['a','b','c'])
+    log.log([1,2,3])
+    log.dump()
+    log.log([1,2,3])
+    log.log([1,2,3])
+    log.dump()
 
 if __name__ == '__main__':
-    l = [0]*10
-    for i in l:
-        i+=10
-    print(l)
+    logger_test()
