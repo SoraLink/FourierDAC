8512dffe04938e4ec017c30f71a7f21378f6c8c8
Codegass <wchweichenhao@gmail.com>
2020-04-28 22:56:15 -0400

    init fourier

---
+++ algo/Fourier.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Four:
+
+    def __init__(self, fft_size=512, sampling_rate=8000):
+        self._fft_size = fft_size
+        self._sampling_rate = sampling_rate
+
+    def fft(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def grad(self):
+        pass
